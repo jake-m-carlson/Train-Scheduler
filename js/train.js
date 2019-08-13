68cdeb0c532fc240bbb4800086f147c0b90ea937
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-08-13 13:11:56 -0500

    button click function

--- js/train.js
+++ js/train.js
@@ -1,5 +1,13 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+// Setup Firebase
+// Create button to add train, push to database, push to table
+// Pull new train from database
+// Calculate next train arrival 
+// Calculate how many minutes away
+
+
+// Setup Firebase
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyCC_gCACqMAEXgCDHYXkkEAzyjKM1nF9CY",
     authDomain: "train-scheduler-pt15.firebaseapp.com",
     databaseURL: "https://train-scheduler-pt15.firebaseio.com",
@@ -7,6 +15,44 @@
     storageBucket: "train-scheduler-pt15.appspot.com",
     messagingSenderId: "296765052302",
     appId: "1:296765052302:web:f441fdb3aa1c736c"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+// Create button to add train, push to database, push to table
+$("#add-train-btn").on("click",function(event) {
+    event.preventDefault();
+
+    // Collect user inputs
+    var trainName = $("train-name-input").val().trim();
+    var trainDestination = $("destination-input").val().trim();
+    var trainTime = $("first-time-input").val().trim();
+    var trainFrequency =$("frequency-input").val().trim();
+
+    // Make temporary local object to store train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+    };
+
+    // Push temp local obj data to firebase
+    database.ref().push(newTrain);
+
+    // Console log everything to log
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.destination);
+    console.log(newTrain.frequency);
+
+    alert("Train added successully!");
+
+    // Clear all the text boxes
+    $("train-name-input").val("");
+    $("destination-input").val("");
+    $("first-time-input").val("");
+    $("frequency-input").val("");
+});
