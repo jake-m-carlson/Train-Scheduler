bf1a0e691041ed08915be265998aa11dacbd15c2
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-08-12 22:57:28 -0500

    linked css and js, added firebase

---
+++ js/train.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCC_gCACqMAEXgCDHYXkkEAzyjKM1nF9CY",
+    authDomain: "train-scheduler-pt15.firebaseapp.com",
+    databaseURL: "https://train-scheduler-pt15.firebaseio.com",
+    projectId: "train-scheduler-pt15",
+    storageBucket: "train-scheduler-pt15.appspot.com",
+    messagingSenderId: "296765052302",
+    appId: "1:296765052302:web:f441fdb3aa1c736c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
